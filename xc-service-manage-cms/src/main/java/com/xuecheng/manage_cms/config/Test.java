1e9d8ff1709861667c5250ed597bb26610b11b27
杜阳 <269598887@qq.com>
2019-07-27 13:18:12 +0800

    add test.java

---
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/config/Test.java
@@ -0,0 +1,10 @@
+package com.xuecheng.manage_cms.config;
+
+public class Test {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int result=a+b;
+        System.out.println(result);
+    }
+}
